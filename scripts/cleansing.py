82b63eb49352760260196646023610efae425e33
vinnibuh <ivchyaroslav@gmail.com>
2021-03-20 18:00:42 +0300

    updated cleansing

--- scripts/cleansing.py
+++ scripts/cleansing.py
@@ -7,14 +7,24 @@ def data_cleaning(df, drop_pid=True, lower_precision=True, drop_rare_values=True
     if drop_pid:
         df = df.drop('PID', axis=1)
         
+    df['MS SubClass'] = df['MS SubClass'].astype('object')
+    print(df['MS SubClass'].dtype)
+        
     # filter outliers
-    cols_to_filter = ['SalePrice', 'Garage Area', 'Total Bsmt SF', 'BsmtFin SF 1', 'BsmtFin SF 2', 'Misc Val']
+    cols_to_filter = ['SalePrice', 'Garage Area', 'Total Bsmt SF', 
+                      'BsmtFin SF 1', 'BsmtFin SF 2', 'Misc Val',
+                      'Pool Area', 'Screen Porch', '3Ssn Porch',
+                      'Enclosed Porch', 'Open Porch SF', 'Wood Deck SF',
+                      'Lot Area', 'Lot Frontage']
+    mask = df['SalePrice'] == df['SalePrice']
     for col in cols_to_filter:
         threshold = df[col].quantile(0.997)
-        df = df.loc[df[col] < threshold, :]
+        mask = mask & ~(df[col] > threshold)
+    df = df.loc[mask, :]
     
     if drop_rare_values:
         df.drop(['Pool QC', 'Misc Feature', 'Alley'], axis=1, inplace=True)
+        df.drop(['3Ssn Porch', 'Pool Area'], axis=1, inplace=True)
     
     float_type = 'float64'
     int_type = 'int64'
@@ -27,12 +37,18 @@ def data_cleaning(df, drop_pid=True, lower_precision=True, drop_rare_values=True
         df.loc[:, int_cols] = df.loc[:, int_cols].astype(new_int_type)
         float_type, int_type = new_float_type, new_int_type
     
+    print(df['MS SubClass'].dtype)
+    
     # separately process columns of different types
     col_by_type = defaultdict(list)
     for index, value in df.dtypes.items():
         col_by_type[str(value)].append(index)
         
-    df.loc[:, col_by_type['object']] = df.loc[:, col_by_type['object']].fillna('missing')
+    cat_feats = col_by_type['object']
+    print(cat_feats)
+    df.loc[:, cat_feats] = df.loc[:, cat_feats].fillna('missing').astype('string')
+    
+    print(df['MS SubClass'].dtype)
     
     # floats
     float_cols = col_by_type[float_type]
@@ -53,5 +69,7 @@ def data_cleaning(df, drop_pid=True, lower_precision=True, drop_rare_values=True
 
     df[few_unique_vals] = df[few_unique_vals].fillna(df.median())
     df[many_unique_vals] = df[many_unique_vals].fillna(df.mean())
+    
+    print(df['MS SubClass'].dtype)
     
     return df
