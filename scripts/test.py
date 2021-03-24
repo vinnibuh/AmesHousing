b1c0aa8b76005aeaf86eb159371895ef57ccef42
vinnibuh <ivchyaroslav@gmail.com>
2021-03-24 21:54:23 +0300

    functioning test.py and train.py

--- scripts/test.py
+++ scripts/test.py
@@ -3,7 +3,7 @@ path.append('..')
 import argparse
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
-import pickle
+import dill
 import warnings
 warnings.filterwarnings('ignore')
 random_state = 17
@@ -12,7 +12,7 @@ random_state = 17
 def main():
     df_test = pd.read_csv(args.test_path)
     X_val, y_val = df_test.drop(['SalePrice'], axis=1), df_test.SalePrice
-    regr = pickle.load(open(args.model_path, 'rb'))
+    regr = dill.load(open(args.model_path, 'rb'))
 
     y_predict = regr.predict(X_val)
     r2_val = r2_score(y_val, y_predict)
@@ -23,7 +23,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="command for testing")
-    parser.add_argument('--model-path', type=str, default='../models/model.pickle', help='location of pretrained model')
+    parser.add_argument('--model-path', type=str, default='../models/model.pk', help='location of pretrained model')
     parser.add_argument('--test-path', type=str, default='../data/test.csv', help='location of test data')
     args = parser.parse_args()
     main()
