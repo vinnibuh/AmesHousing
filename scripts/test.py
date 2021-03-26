37472a00dbc44237842df6d3a29e2975c7fe0b3d
vinnibuh <ivchyaroslav@gmail.com>
2021-03-26 22:39:40 +0300

    fixed pickling and some bugs

--- scripts/test.py
+++ scripts/test.py
@@ -3,7 +3,7 @@ path.append('..')
 import argparse
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
-import dill
+import pickle
 import warnings
 warnings.filterwarnings('ignore')
 random_state = 17
@@ -12,7 +12,7 @@ random_state = 17
 def main():
     df_test = pd.read_csv(args.test_path)
     X_val, y_val = df_test.drop(['SalePrice'], axis=1), df_test.SalePrice
-    regr = dill.load(open(args.model_path, 'rb'))
+    regr = pickle.load(open(args.model_path, 'rb'))
 
     y_predict = regr.predict(X_val)
     r2_val = r2_score(y_val, y_predict)
