32c753a610fd390acb64477b427ced5a5b3faa24
vinnibuh <ivchyaroslav@gmail.com>
2021-03-29 01:05:51 +0300

    updated project version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Ivchenkov Yaroslav'
 author = 'Ivchenkov Yaroslav'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
