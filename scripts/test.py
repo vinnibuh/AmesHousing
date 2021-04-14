90b233f5ef6ce60df1225c5128b3648487716f53
vinnibuh <ivchyaroslav@gmail.com>
2021-04-14 15:49:29 +0300

    update scripts flags

--- scripts/test.py
+++ scripts/test.py
@@ -11,7 +11,7 @@ random_state = 17
 def main():
     print('Testing model')
     X_val, y_val = csv_to_samples(args.data_path)
-    model = pickle.load(open(args.input_path, 'rb'))
+    model = pickle.load(open(args.model_path, 'rb'))
     results = func.test(X_val, y_val, model)
     print('Validation MSLE value: ', results['MSLE'])
     print('Validation R2 value: ', results['R2'])
@@ -19,7 +19,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="command for testing")
-    parser.add_argument('-i', '--input-path', type=str, default='./models/model.pk',
+    parser.add_argument('-m', '--model-path', type=str, default='./models/model.pk',
                         help='path to pretrained model file made by train.py')
     parser.add_argument('-d', '--data-path', type=str, default='./data/test.csv',
                         help='path to train data made by utils.split_dataset')
